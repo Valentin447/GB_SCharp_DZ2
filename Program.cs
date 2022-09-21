c42b95ce4791152f51485062ce68d52e517c1635
Valentin447 <valentin@myttk.ru>
2022-09-21 19:51:32 +0700

    Add solution Task10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿void Task10(){
+    // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(Math.Abs(number % 100 / 10));
 }
 void Task13(){
 
@@ -9,5 +14,5 @@ void Task15(){
 }
 
 Task10();
-Task13();
-Task15();
+//Task13();
+//Task15();
