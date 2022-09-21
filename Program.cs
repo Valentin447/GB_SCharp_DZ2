825c88583832edc4d5363024e14bd21e88904bec
Valentin447 <valentin@myttk.ru>
2022-09-21 20:18:50 +0700

    Add solution task13

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿void Task10(){
+﻿void Task10()
+{
     // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
     Console.Write("Введите трехзначное число: ");
@@ -6,13 +7,35 @@
 
     Console.WriteLine(Math.Abs(number % 100 / 10));
 }
-void Task13(){
+void Task13()
+{
+    //Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int numberAbs = Math.Abs(number);
+
+    if (numberAbs > 99)
+    {
+        while (numberAbs > 999)
+        {
+            numberAbs = numberAbs / 10;
+        }
+
+        Console.WriteLine(numberAbs % 10);
+    } 
+    else 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
 
 }
-void Task15(){
+void Task15()
+{
 
 }
 
-Task10();
-//Task13();
+//Task10();
+Task13();
 //Task15();
